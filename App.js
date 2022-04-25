cc8613d7d18bf8e81a5d4a43b48f6e5ca743e1c0
iradukundacyuzuzo50@gmail.com <root@Andelas-MacBook-Pro.local>
2022-04-25 11:56:52 +0200

    Connected to Bd Mongoose And Crud using App.js

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 const express = require('express');
-const req = require('express/lib/request');
-
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 //express app
 const app  = express();
+
+//CONECT DB
+const dbURI = 'mongodb+srv://BenIraa:keepitsafe@cluster0.tplc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true  })
+   .then((results) => console.log('DB CONNECTED'))
+   .catch((err) => console.error(err))
+
 
 //register view engine
 app.set('view engine', 'ejs');
 
 // listen for request
 app.listen(3000);
+//middleware $ static filesecure
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+
+
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
-        {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
-        {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
-        {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
-    ];
+    res.redirect('/blogs'); 
+    // const blogs = [
+    //     {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
+    //     {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
+    //     {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
+    //     {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
+    // ];
 
     // res.send('<p>Boost website Api</p>');
     // res.sendFile('./src/index.html', {root: __dirname });
@@ -28,14 +43,18 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About'})
 
 });
+//blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort(( {createdAt: -1}))
+    .then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result})
+
+    })
+    .catch((err) => {console.log(err);})
+})
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'New Blog'});
 })
-
-
-
-
-
 
 
 //404 Page should be on bottom
