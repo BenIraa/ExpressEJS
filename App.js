746aa5d8846e6b407a3e9b3516c2cc1d9275c7d0
iradukundacyuzuzo50@gmail.com <root@Andelas-MacBook-Pro.local>
2022-04-25 15:11:45 +0200

    ft delete

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const { render } = require('express/lib/response');
 //express app
 const app  = express();
 
@@ -17,21 +18,16 @@ app.set('view engine', 'ejs');
 
 // listen for request
 app.listen(3000);
+
 //middleware $ static filesecure
 app.use(express.static('public'));
+app.use(express.urlencoded( { extended: true } ));
 app.use(morgan('dev'));
 
 
 
 app.get('/', (req, res) => {
     res.redirect('/blogs'); 
-    // const blogs = [
-    //     {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
-    //     {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
-    //     {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
-    //     {title: 'first blog', snippet: ' lorem ihsasas hbshvds sbdhsbd'},
-    // ];
-
     // res.send('<p>Boost website Api</p>');
     // res.sendFile('./src/index.html', {root: __dirname });
     res.render('index', { title: 'Home', blogs});
@@ -44,6 +40,10 @@ app.get('/about', (req, res) => {
 
 });
 //blog routes
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'New Blog'});
+})
+
 app.get('/blogs', (req, res) => {
     Blog.find().sort(( {createdAt: -1}))
     .then((result) => {
@@ -52,9 +52,37 @@ app.get('/blogs', (req, res) => {
     })
     .catch((err) => {console.log(err);})
 })
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'New Blog'});
+app.post('/blogs', (req, res) => {
+    // console.log(req.body);
+    const blog = new Blog(req.body);
+
+    blog.save()
+       .then((result) => {
+           res.redirect('/blogs')
+       })
+       .catch((err) => {console.log(err);})
 })
+app.get('/blogs/:id', (req, res) => {
+    const id =  req.params.id;
+    console.log(id);
+    Blog.findById(id)
+      .then((result) => {
+        res.render('details', {blog: result, title: 'Blog details'})
+      })
+      .catch((err) => {console.log(err);});
+})
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+      .then(result => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
 
 
 //404 Page should be on bottom
