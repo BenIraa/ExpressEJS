80d7e4564042fc3061058c84a3e36e7f5b204062
iradukundacyuzuzo50@gmail.com <root@Andelas-MacBook-Pro.local>
2022-04-26 11:02:09 +0200

    Separation

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
 const { render } = require('express/lib/response');
+const blogRoutes = require('./routes/blogRoutes');
+
+
 //express app
 const app  = express();
 
@@ -39,50 +41,8 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About'})
 
 });
-//blog routes
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'New Blog'});
-})
 
-app.get('/blogs', (req, res) => {
-    Blog.find().sort(( {createdAt: -1}))
-    .then((result) => {
-        res.render('index', {title: 'All Blogs', blogs: result})
-
-    })
-    .catch((err) => {console.log(err);})
-})
-app.post('/blogs', (req, res) => {
-    // console.log(req.body);
-    const blog = new Blog(req.body);
-
-    blog.save()
-       .then((result) => {
-           res.redirect('/blogs')
-       })
-       .catch((err) => {console.log(err);})
-})
-app.get('/blogs/:id', (req, res) => {
-    const id =  req.params.id;
-    console.log(id);
-    Blog.findById(id)
-      .then((result) => {
-        res.render('details', {blog: result, title: 'Blog details'})
-      })
-      .catch((err) => {console.log(err);});
-})
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Blog.findByIdAndDelete(id)
-      .then(result => {
-        res.json({ redirect: '/blogs' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-
+app.use('/blogs', blogRoutes)
 
 
 //404 Page should be on bottom
